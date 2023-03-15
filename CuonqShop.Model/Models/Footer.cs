224cc4ec3d54b592194c1a5271f43eef405d11c6
Qcuong103 <quoccuong100300@gmail.com>
2023-03-15 23:19:25 +0700

    mod model footer

---
+++ CuonqShop.Model/Models/Footer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuonqShop.Model.Models
+{
+    [Table("Footer")]
+    public class Footer
+    {
+        [Key]
+        public string ID { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
