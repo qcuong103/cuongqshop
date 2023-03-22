520832a4637b0b2c4149ccd1fc10accd15e59691
Qcuong103 <quoccuong100300@gmail.com>
2023-03-22 16:02:41 +0700

    .

---
+++ CuongqShop.Data/Repositories/ErrorRepository.cs
@@ -0,0 +1,16 @@
+﻿using CuongqShop.Data.Infrastructure;
+using CuongqShop.Model.Models;
+
+namespace CuongqShop.Data.Repositories
+{
+    public interface IErrorRepository : IRepository<Error>
+    {
+    }
+
+    public class ErrorRepository : RepositoryBase<Error>, IErrorRepository
+    {
+        public ErrorRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
---
+++ CuongqShop.Model/Models/Page.cs
@@ -0,0 +1,25 @@
+﻿using CuongqShop.Model.Abtract;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CuongqShop.Model.Models
+{
+    [Table("Pages")]
+    public class Page : Auditable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(256)]
+        public string Name { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [MaxLength(256)]
+        [Required]
+        public string Alias { set; get; }
+
+        public string Content { get; set; }
+    }
+}
